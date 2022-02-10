1d4e9b6b3cb0adb16b8de821bce880a991e88def
Md-Raihan-Alam <mdraihanalam40@gmail.com>
2022-02-10 19:48:41 +0600

    solved
    some comments added

--- Replicate New.js
+++ Replicate New.js
@@ -1,8 +1,9 @@
 ///Read this->https://stackoverflow.com/questions/1646698/what-is-the-new-keyword-in-javascript
+//https://zeekat.nl/articles/constructors-considered-mildly-confusing.html
 function nouveau(func) {
     var res = {};
     if (func.prototype !== null) {
-        res.__proto__ = func.prototype;
+        res.__proto__ = func.prototype;//__proto__ is a property, in every JavaScript object, which  points to object 's constructor function's prototype property
     }
     var ret = func.apply(res, Array.prototype.slice.call(arguments, 1));
     if ((typeof ret === "object" || typeof ret === "function") && ret !== null) {
